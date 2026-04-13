5853da614ce7699bacb1576982b8a0c1ad7514c2
Mikail Dzaitov <dlametaverse76@gmail.com>
2026-04-13 19:28:55 +0300

    add project v2

---
+++ src/dll/useTrackSelection.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+
+export function useTrackSelection() {
+  const [trackId, setTrackId] = useState<string | null>(null);
+
+  return { trackId, setTrackId };
+}
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import { MainPage } from "./ui/MainPage";
+import "./index.css";
+
+createRoot(document.getElementById('root')!).render(
+    <MainPage />
+)
+
+
